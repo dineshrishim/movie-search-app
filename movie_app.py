173f1bd6bf2d3d6cc41ca3c70d814e33ec3f3433
Dinesh Mandapalli <dmandapa@ucsc.edu>
2026-04-20 15:07:22 -0700

    movie search app with streamlit + omdb api

---
+++ movie_app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import requests
+
+st.title("BROOOOOO Dis da Movie Search App")
+
+movie = st.text_input("Enter da movie name:")
+
+if movie:
+    api_key = "7fa6ccf5"
+
+    url = f"http://www.omdbapi.com/?t={movie}&apikey={api_key}"
+
+    response = requests.get(url)
+    data = response.json()
+
+    if data.get("Response") == "False":
+        st.write("Movie ain't found bro")
+    else:
+        st.subheader(data["Title"])
+        st.write("Year:", data["Year"])
+        st.write("Rating:", data["imdbRating"])
+        st.write("Plot:", data["Plot"])
+        st.image(data["Poster"])
